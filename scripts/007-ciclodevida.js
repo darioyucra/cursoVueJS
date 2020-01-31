b3565fb611a835abdeb9d80cd46fcac9e828fb25
Dario Yucra <dario.yucra@corebizglobal.com>
2020-01-31 18:40:06 -0300

    initial commit

---
+++ scripts/007-ciclodevida.js
@@ -0,0 +1,36 @@
+const app = new Vue({
+
+    el: '#app',
+    data: {
+        saludo: 'soy ciclo de vida de VUE'
+    },
+    beforeCreate() {
+        console.log('before create');
+    },
+    created(){
+        console.log('created');
+    },
+    beforeMount(){
+        console.log('beforeMount');
+    },
+    mounted(){
+        console.log('mounted');
+    },
+    beforeUpdate(){
+        console.log('beforeUpdate');
+    },
+    updated(){
+        console.log('updated');
+    },
+    beforeDestroy(){
+        console.log('beforeDestroy');
+    },
+    destroyed(){
+        console.log('destroyed');
+    },
+    methods: {
+        destruir(){
+            this.$destroy();
+        }
+    }
+});
